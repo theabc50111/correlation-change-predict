e51467c01df671c0afac3a670f56f2f0762f4e1d
YWT <theabc50111@gmail.com>
2023-03-25 17:56:34 +0800

    update model operate time in schedule_experiments.py

--- mts_corr_ad_model/schedule_experiments.py
+++ mts_corr_ad_model/schedule_experiments.py
@@ -29,18 +29,20 @@ args_list = sorted(args_list, key=lambda x:x[8])
 args_list = sorted(args_list, key=lambda x:x[2])
 
 num_models = sum([1 for x in args_list if x[2]=="--discr_loss true" and  x[8]==5 and x[9]==16])  # the main reasons for model operation time: discr_loss, gra_enc_l, gra_enc_h
-model_timedelta_list = [timedelta(minutes=20), timedelta(hours=2), timedelta(hours=2), timedelta(hours=2, minutes=40),  # The order of elements of model_timedelta_list should comply with the order of comply with args_list
+model_timedelta_list = [timedelta(minutes=20), timedelta(minutes=45), timedelta(hours=1), timedelta(hours=1, minutes=20),  # The order of elements of model_timedelta_list should comply with the order of comply with args_list
                         timedelta(hours=2, minutes=30), timedelta(hours=3, minutes=35), timedelta(hours=7, minutes=30), timedelta(hours=8)]
 model_timedelta_list = list(chain.from_iterable(repeat(x, num_models) for x in model_timedelta_list))
+model_timedelta_list = [0] + model_timedelta_list
+model_timedelta_list.pop()
 assert len(args_list) == len(model_timedelta_list), "The order of elements of model_timedelta_list should comply with the order of comply with args_list"
 print(f"# len of experiments: {len(args_list)}")
 
-#start_t = datetime.now() + timedelta(minutes = 10)
-start_t = datetime.now() - timedelta(minutes = 55)
-for i, (t_delta, model_args) in enumerate(zip(model_timedelta_list, args_list)):
-    # print(t_delta, model_args)
-    t = start_t if i==0 else t+t_delta
-    args = [t.strftime("%M %H %d %m") + " *"] + list(model_args)
-    print("{} /home/ywt01_dmlab/Documents/codes/correlation-change-predict/mts_corr_ad_model/crontab_mts_corr_ad_model.sh {} --filt_quan {} {} --discr_loss_r {} --discr_pred_disp_r {} {} --drop_p {} --gra_enc {} --gra_enc_l {} --gra_enc_h {} --save_model true".format(*args))
+#experiments_start_t = datetime.now() + timedelta(minutes = 10)
+experiments_start_t = datetime.now() - timedelta(hours=18, minutes=35)
+for i, (prev_model_time_len, model_args) in enumerate(zip(model_timedelta_list, args_list)):
+    # print({"operate time length of previous model": prev_model_time_len, "model argumets": model_args})
+    model_start_t = experiments_start_t if i==0 else model_start_t + prev_model_time_len
+    cron_args = [model_start_t.strftime("%M %H %d %m") + " *"] + list(model_args)
+    print("{} /home/ywt01_dmlab/Documents/codes/correlation-change-predict/mts_corr_ad_model/crontab_mts_corr_ad_model.sh {} --filt_quan {} {} --discr_loss_r {} --discr_pred_disp_r {} {} --drop_p {} --gra_enc {} --gra_enc_l {} --gra_enc_h {} --save_model true".format(*cron_args))
     # if x[8]==1 and x[9]==4:
-    #     print(t_delta, model_args)
+    #     print(prev_model_time_len, model_args)
