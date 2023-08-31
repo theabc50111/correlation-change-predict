b716ef87ceec75f615a98d626df1ce86bad2f8e0
YWT <theabc50111@gmail.com>
2023-09-01 02:46:24 +0800

    Revise correlation_data_etl.ipynb & Revise options of schedule_experiments.py

--- mts_corr_ad_model/schedule_experiments.py
+++ mts_corr_ad_model/schedule_experiments.py
@@ -4,9 +4,9 @@ from datetime import datetime, timedelta
 from itertools import chain, product, repeat
 from pprint import pprint
 
-data_implement_list = ["--data_implement SP500_20112015_CORR_SER_REG_STD_CORR_MAT_LARGE_FILTERED_HRCHY_10_CLUSTER_LABEL_LAST_V2"]  # "--data_implement LINEAR_REG_ONE_CLUSTER_DIM_30_BKPS_0_NOISE_STD_30"
+data_implement_list = ["--data_implement SP500_20112015_CORR_SER_REG_STD_CORR_MAT_LARGE_FILTERED_HRCHY_10_CLUSTER_LABEL_LAST"]  # "--data_implement LINEAR_REG_ONE_CLUSTER_DIM_30_BKPS_0_NOISE_STD_30"
 batch_size_list = [""]
-train_models_list = ["--train_models CLASSMTSCORRAD"]  # ["", "--train_models MTSCORRAD", "--train_models MTSCORRAD --train_models BASELINE", "--train_models MTSORRAD --train_models BASELINE --train_models GAE"]
+train_models_list = ["--train_models CLASSBASELINE"]  # ["", "--train_models MTSCORRAD", "--train_models MTSCORRAD --train_models BASELINE", "--train_models MTSORRAD --train_models BASELINE --train_models GAE"]
 corr_type_list = ["--corr_type pearson"]  # ["--corr_type pearson", "--corr_type cross_corr"]
 seq_len_list = ["--seq_len 30"]  # ["--seq_len 5", "--seq_len 10"]
 filt_mode_list = [""]  # ["", "--filt_mode keep_strong", "--filt_mode keep_positive", "--filt_mode keep_abs"]
@@ -22,11 +22,11 @@ learning_rate_list = [""]  # ["--learn_rate 0.0001", "--learn_rate 0.0005", "--l
 weight_decay_list = [""]  # ["--weight_decay 0.0001", "--weight_decay 0.0005", "--weight_decay 0.001", "--weight_decay 0.005", "--weight_decay 0.01", "--weight_decay 0.05", "--weight_decay 0.1"]
 graph_enc_weight_l2_reg_lambda_list = [""]  # ["", "--graph_enc_weight_l2_reg_lambda 0.01", "--graph_enc_weight_l2_reg_lambda 0.001"]
 drop_pos_list = ["", "--drop_pos class_fc"]  # ["", "--drop_pos gru", "--drop_pos decoder --drop_pos gru", "--drop_pos gru --drop_pos decoder --drop_pos graph_encoder"]
-drop_p_list = ["", "--drop_p 0.05", "--drop_p 0.1", "--drop_p 0.15", "--drop_p 0.2"]  # ["--drop_p 0.33", "--drop_p 0.5", "--drop_p 0.66"]
+drop_p_list = ["--drop_p 0.05", "--drop_p 0.1", "--drop_p 0.15", "--drop_p 0.2", "--drop_p 0.3", "--drop_p 0.4", "--drop_p 0.5"]  # ["--drop_p 0.33", "--drop_p 0.5", "--drop_p 0.66"]
 gra_enc_list = [""]  # ["", "--gra_enc gin", "--gra_enc gine"]
 gra_enc_aggr_list = [""]  # ["", "--gra_enc_aggr mean", "--gra_enc_aggr add", "--gra_enc_aggr max"]
-gra_enc_l_list = ["--gra_enc_l 2"]  # ["--gra_enc_l 1", "--gra_enc_l 2", "--gra_enc_l 3", "--gra_enc_l 4", "--gra_enc_l 5"]
-gra_enc_h_list = ["--gra_enc_h 16"]  # ["--gra_enc_h 32", "--gra_enc_h 64", "--gra_enc_h 128", "--gra_enc_h 256", "--gra_enc_h 512"]
+gra_enc_l_list = [""]  # ["--gra_enc_l 1", "--gra_enc_l 2", "--gra_enc_l 3", "--gra_enc_l 4", "--gra_enc_l 5"]
+gra_enc_h_list = [""]  # ["--gra_enc_h 32", "--gra_enc_h 64", "--gra_enc_h 128", "--gra_enc_h 256", "--gra_enc_h 512"]
 gra_enc_mlp_l_list = [""]  # ["--gra_enc_mlp_l 1", "--gra_enc_mlp_l 2", "--gra_enc_mlp_l 3"]
 gru_l_list = [""]  # ["--gru_l 1", "--gru_l 2", "--gru_l 3", "--gru_l 4", "--gru_l 5"]
 gru_h_list = [""]  # ["--gru_h 40", "--gru_h 80", "--gru_h 100", "--gru_h 320", "--gru_h 640"]
@@ -69,7 +69,7 @@ elif set(map(lambda x: x['gru_l'], args_list)) != {""}:
     gru_l_values_set.discard("")
     gru_l_pop_value = gru_l_values_set.pop()
     num_models = sum(1 for x in args_list if x['gru_l'] == gru_l_pop_value)  # the main reasons for model operation time: discr_loss, gra_enc_l, gra_enc_h
-    model_timedelta_list = [timedelta(hours=3, minutes=10)]  # The order of elements of model_timedelta_list should comply with the order of elements of args_list
+    model_timedelta_list = [timedelta(hours=1, minutes=25)]  # The order of elements of model_timedelta_list should comply with the order of elements of args_list
 else:
     num_models = len(args_list)
     model_timedelta_list = [timedelta(hours=2, minutes=40)]
