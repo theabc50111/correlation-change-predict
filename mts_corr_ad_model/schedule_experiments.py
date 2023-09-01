5b01542cda96d4a8c9623744a0b3837388db2f54
YWT <theabc50111@gmail.com>
2023-09-01 16:30:04 +0800

    Revise filtered_distance_mat_method correlation_data_etl.ipynb

--- mts_corr_ad_model/schedule_experiments.py
+++ mts_corr_ad_model/schedule_experiments.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 from itertools import chain, product, repeat
 from pprint import pprint
 
-data_implement_list = ["--data_implement SP500_20112015_CORR_SER_REG_STD_CORR_MAT_LARGE_FILTERED_HRCHY_10_CLUSTER_LABEL_LAST"]  # "--data_implement LINEAR_REG_ONE_CLUSTER_DIM_30_BKPS_0_NOISE_STD_30"
+data_implement_list = ["--data_implement SP500_20112015_CORR_SER_REG_STD_CORR_MAT_LARGE_FILTERED_HRCHY_10_CLUSTER_LABEL_LAST_V2"]  # "--data_implement LINEAR_REG_ONE_CLUSTER_DIM_30_BKPS_0_NOISE_STD_30"
 batch_size_list = [""]
 train_models_list = ["--train_models CLASSBASELINE"]  # ["", "--train_models MTSCORRAD", "--train_models MTSCORRAD --train_models BASELINE", "--train_models MTSORRAD --train_models BASELINE --train_models GAE"]
 corr_type_list = ["--corr_type pearson"]  # ["--corr_type pearson", "--corr_type cross_corr"]
