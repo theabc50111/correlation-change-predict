86f180b8327bc0546d3ef29dba81bae59aff4136
YWT <theabc50111@gmail.com>
2023-10-16 14:49:43 +0800

    Revise scheduler_experiments

--- mts_corr_ad_model/schedule_experiments.py
+++ mts_corr_ad_model/schedule_experiments.py
@@ -4,9 +4,9 @@ from datetime import datetime, timedelta
 from itertools import chain, product, repeat
 from pprint import pprint
 
-data_implement_list = ["--data_implement SP500_20112015_CORR_SER_REG_STD_CORR_MAT_IRRELEVANT_FILTERED_HRCHY_6_CLUSTER_LABEL_1TH"]  # "--data_implement LINEAR_REG_ONE_CLUSTER_DIM_30_BKPS_0_NOISE_STD_30"
+data_implement_list = ["--data_implement "]  # "--data_implement LINEAR_REG_ONE_CLUSTER_DIM_30_BKPS_0_NOISE_STD_30"
 batch_size_list = [""]
-train_models_list = ["--train_models CLASSMTSCORRAD"]  # ["", "--train_models MTSCORRAD", "--train_models MTSCORRAD --train_models BASELINE", "--train_models MTSORRAD --train_models BASELINE --train_models GAE"]
+train_models_list = ["--train_models CLASSBASELINECUSTOMFEATURE"]  # ["", "--train_models MTSCORRAD", "--train_models MTSCORRAD --train_models BASELINE", "--train_models MTSORRAD --train_models BASELINE --train_models GAE"]
 corr_type_list = ["--corr_type pearson"]  # ["--corr_type pearson", "--corr_type cross_corr"]
 seq_len_list = ["--seq_len 30"]  # ["--seq_len 5", "--seq_len 10"]
 filt_mode_list = [""]  # ["", "--filt_mode keep_strong", "--filt_mode keep_positive", "--filt_mode keep_abs"]
@@ -31,7 +31,7 @@ gra_enc_h_list = [""]  # ["--gra_enc_h 32", "--gra_enc_h 64", "--gra_enc_h 128",
 gra_enc_mlp_l_list = [""]  # ["--gra_enc_mlp_l 1", "--gra_enc_mlp_l 2", "--gra_enc_mlp_l 3"]
 gru_l_list = [""]  # ["--gru_l 1", "--gru_l 2", "--gru_l 3", "--gru_l 4", "--gru_l 5"]
 gru_h_list = [""]  # ["--gru_h 40", "--gru_h 80", "--gru_h 100", "--gru_h 320", "--gru_h 640"]
-gru_input_feature_idx_list = [""]  # ["--gru_input_feature_idx 0", "--gru_input_feature_idx 1", "--gru_input_feature_idx 2", "--gru_input_feature_idx 0 --gru_input_feature_idx 1 "]
+gru_input_feature_idx_list = ["--gru_input_feature_idx 2 --gru_input_feature_idx 3 --gru_input_feature_idx 4 --gru_input_feature_idx 5 --gru_input_feature_idx 7 --gru_input_feature_idx 8 --gru_input_feature_idx 9 --gru_input_feature_idx 10 --gru_input_feature_idx 11 --gru_input_feature_idx 12  --gru_input_feature_idx 13  --gru_input_feature_idx 14"]  # ["--gru_input_feature_idx 0", "--gru_input_feature_idx 1", "--gru_input_feature_idx 2", "--gru_input_feature_idx 0 --gru_input_feature_idx 1 "]
 use_weighted_loss_list = ["", "--use_weighted_loss true"]  # ["", "--use_weighted_loss true"]
 edge_acc_loss_atol_list = [""]  # ["", "--edge_acc_loss_atol 0.05", "--edge_acc_loss_atol 0.1", "--edge_acc_loss_atol 0.33"]
 two_ord_pred_prob_edge_accu_thres_list = [""]  # ["", "--two_ord_pred_prob_edge_accu_thres 0.1", "--two_ord_pred_prob_edge_accu_thres 0.2"]
